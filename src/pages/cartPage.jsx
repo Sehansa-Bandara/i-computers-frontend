8c29041b2deb7c0f2f84d38a44e214b66e6e2ece
Sehansa Bandara <sehadil10953@gmail.com>
2026-08-11 00:38:44 +0530

    🛒✨

--- src/pages/cartPage.jsx
+++ src/pages/cartPage.jsx
@@ -83,7 +83,7 @@ export default function CartPage() {
                                     </div>
                                 </div>
                             </div>
-                            
+
                         );
                     })}
                 </div>
